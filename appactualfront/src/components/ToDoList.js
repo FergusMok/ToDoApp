67a6b949b50da0dd02d8742b24611ffd8487ab4b
FergusMok <FergusMok1@gmail.com>
2020-12-22 17:44:59 +0800

    get destroy's button link to work

--- appactualfront/src/components/ToDoList.js
+++ appactualfront/src/components/ToDoList.js
@@ -4,34 +4,40 @@ import {useDispatch, useSelector} from 'react-redux'
 import axios from 'axios'
 import {change_db, destroydb} from '../redux/database'
 
-const API_LINK = 'http://localhost:5000//api/v1/items.json'
+const API_LINK = 'http://localhost:5000//api/v1/items'
 
 const ToDoList = () => {
 
     const dispatch = useDispatch();
 
     const getDatabase = async () => {
-        const database = await axios.get(API_LINK)
+        const database = await axios.get(API_LINK + ".json")
         dispatch(change_db(database.data.data))
     }
 
-    const renderDatabase = useSelector( state => state.getdestroy ).map( 
+    const deleteEntry = async db => {
+        const lastEntryId = db[0].id
+        console.log(API_LINK+"/"+lastEntryId)
+
+        await axios.destroy(API_LINK+"/"+lastEntryId)
+        getDatabase() // Delete, and re-render the database.
+    }
+
+    const currentDatabase = useSelector( state => state.getdestroy )
+    const renderDatabase = currentDatabase.map( 
         jsonObject => {
-            return (<div>
+            return (<div key = {jsonObject.id}>
                 {jsonObject.title} {jsonObject.body}
             </div>)
         })
 
     return ( <div>
-
-            <button class="ui button" onClick = {() => getDatabase()}>
+            <button className="ui button" onClick = {() => getDatabase()}>
                 LOAD!
             </button>
-
-            <div class="ui button" tabindex="0">
-                 Delete the earliest entry
+            <div className="ui button" tabIndex="0" onClick = {() => deleteEntry(currentDatabase)}>
+                 Delete the first entry
             </div>
-            
             <div> {renderDatabase} </div>
 
             </div>)
