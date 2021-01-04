b78b991a476b3b30f809ca5611c5d2416a7f028a
FergusMok <FergusMok1@gmail.com>
2021-01-04 21:42:44 +0800

    Finished with Docker, FilterBar halfway

--- appfrontend/src/components/FilterBar.js
+++ appfrontend/src/components/FilterBar.js
@@ -1,7 +1,5 @@
-
-// NOT IMPLEMENTED YET
 import React, {useEffect, useState, useCallback} from 'react'
-import {useDispatch, useSelector} from 'react-redux'
+import {useSelector} from 'react-redux'
 import { Dropdown } from 'semantic-ui-react'
 
 const FilterBar = ({match}) => {
@@ -9,16 +7,14 @@ const FilterBar = ({match}) => {
     const currentDatabase = useSelector(state => state.databaseState)
     const [tagState, setTags] = useState([]);
     const [currentTag, setCurrentTag] = useState([])
+    const isCompleted = useCallback( () => match.path === "/completed", [match] )
 
-    const isCompleted = match.path === "/completed"
-
-    useEffect( () => {
-        const refreshFilterBar = async () => {
+    useEffect( () => { // Parse through the available tags, and display them as options
+        const refreshFilterBar = () => {
             // Unique tagsList 
             const tagsList = [... new Set(currentDatabase.
-                filter(jsonObject => jsonObject.completed === isCompleted && jsonObject.tag_list.length > 0).
+                filter(jsonObject => jsonObject.completed === isCompleted() && jsonObject.tag_list.length > 0).
                 map(jsonObject => jsonObject.tag_list)) ]
-            
             setTags( tagsList.flat().map( tag => {
                 const newObj = {key: tag, text: tag, value: tag} 
                 return newObj
@@ -26,14 +22,20 @@ const FilterBar = ({match}) => {
         }
         refreshFilterBar();
     },[currentDatabase] )
-    
+
+    useEffect( () => {
+        setCurrentTag([]) // Reset the dropdown animation when change to a new page
+    }, [match])
+
+    console.log(currentTag)
     return (
         <Dropdown 
-        placeholder='Skills' 
-        fluid 
-        multiple 
-        selection 
-        options={tagState} 
+        placeholder='Skills'
+        fluid
+        multiple
+        selection
+        options={tagState}
+        value = {currentTag}
         onChange={(event, {value}) => {setCurrentTag(value)}}
         />
     )
