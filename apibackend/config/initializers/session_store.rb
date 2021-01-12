8474b58c4a4ba3445e2993d24f2e344d4719e88c
FergusMok <FergusMok1@gmail.com>
2021-01-13 00:57:18 +0800

    Installed same site cookie gem

--- apibackend/config/initializers/session_store.rb
+++ apibackend/config/initializers/session_store.rb
@@ -1,4 +1,4 @@
-=begin if Rails.env == "production" # For production
+if Rails.env == "production" # For production
     Rails.application.config.session_store :cookie_store, expire_after: 3.days,
     key: "_authentication_app", 
     domain: "https://fergus-cvwo.netlify.app",
@@ -6,13 +6,8 @@
     secure: true,
     httponly: false,
     same_site: :none
-
 else
     # If in development, don't care about domain.
     Rails.application.config.session_store :cookie_store, expire_after: 3.days,
     key: "_authentication_app"
-end 
-=end
-
-Rails.application.config.session_store :cookie_store, expire_after: 3.days,
-    key: "_authentication_app"
+end
