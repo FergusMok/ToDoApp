4f6bb19403d29375dfe0ecfb3e27ae19807a3f61
FergusMok <FergusMok1@gmail.com>
2021-01-13 13:26:36 +0800

    enabled httponly

--- apibackend/config/initializers/session_store.rb
+++ apibackend/config/initializers/session_store.rb
@@ -1,13 +1,14 @@
 if Rails.env == "production" # For production
-    Rails.application.config.session_store(:cookie_store, same_site: :none): 3.days,
-    key: "_authentication_app",
+    Rails.application.config.session_store :cookie_store,  expire_after: 3.days,
+    key: "_authentication_app", 
+    domain: "https://fergus-cvwo.netlify.app",
     header: :any,
     secure: true,
-    httponly: false,
+    httponly: true,
     same_site: :none
 else
     # If in development, don't care about domain.
-    Rails.application.config.session_store(:cookie_store, same_site: :none): 3.days,
+    Rails.application.config.session_store :cookie_store, expire_after: 3.days,
     key: "_authentication_app",
     secure: true
 end
