245ece2130851695c755e484afc86acb634b4df5
FergusMok <FergusMok1@gmail.com>
2021-01-13 13:10:42 +0800

    Try rack 2.1 :same_site configuration

--- apibackend/config/initializers/session_store.rb
+++ apibackend/config/initializers/session_store.rb
@@ -1,5 +1,5 @@
 if Rails.env == "production" # For production
-    Rails.application.config.session_store :cookie_store, expire_after: 3.days,
+    Rails.application.config.session_store(:cookie_store, same_site: :none): 3.days,
     key: "_authentication_app", 
     domain: "https://fergus-cvwo.netlify.app",
     header: :any,
@@ -8,6 +8,7 @@ if Rails.env == "production" # For production
     same_site: :none
 else
     # If in development, don't care about domain.
-    Rails.application.config.session_store :cookie_store, expire_after: 3.days,
-    key: "_authentication_app"
+    Rails.application.config.session_store(:cookie_store, same_site: :none): 3.days,
+    key: "_authentication_app",
+    secure: true
 end
