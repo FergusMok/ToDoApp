faddc2616a940e7d8f318b9e2aeb5a1f36b15151
FergusMok <FergusMok1@gmail.com>
2021-01-15 10:15:54 +0800

    Added whenver gem, set up cron job

--- apibackend/app/models/user.rb
+++ apibackend/app/models/user.rb
@@ -5,4 +5,12 @@ class User < ApplicationRecord
     validates :password, presence: true
     validates :name, presence: true
     validates :email, presence: true, uniqueness: true, case_sensitive: false, format: URI::MailTo::EMAIL_REGEXP
+    
+    
+    def emailUsers
+        User.find_each do |user|
+            UserMailer.due_date_chaser(user).deliver_now
+    end
+
+
 end
